f0b4e631fddd1b5dd9c26bd057301d930aacd25c
Debora Benedetto <deb.benedetto@stud.uniroma3.it>
2019-06-06 11:45:58 +0200

    add file install-gradle.sh

---
+++ travis_scripts/install-gradle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#install gradle 5.0 (default 4.0)
+wget https://services.gradle.org/distributions/gradle-5.0-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-*.zip
+ls /opt/gradle/gradle-5.0
+echo $GRADLE_HOME
+export GRADLE_HOME=/opt/gradle/gradle-5.0
+export PATH=$GRADLE_HOME/bin:$PATH
+echo $GRADLE_HOME
